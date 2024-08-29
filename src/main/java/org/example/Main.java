09a77edd0d057ea4731eb84dcb345a496ba324a5
Nora Kauczor <n.kauczor@web.de>
2024-08-29 15:21:47 +0200

    override and test constructor method of order

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -8,27 +9,42 @@ public class Main {
     public static void main(String[] args) {
         OrderMapRepo myOrderMapRepo = new OrderMapRepo(new HashMap<>());
         ShopService myShopService = new ShopService(myOrderMapRepo);
-
+        // Create products
         Product tv = new Product(149384904, "UltraHD Smart TV", "TechVision", 599.99);
         Product earbuds = new Product(24342421, "Wireless Earbuds", "SoundWave", 79.99);
         Product laptop = new Product(357467674, "Gaming Laptop", "PowerGamer", 1299.99);
         Product blender = new Product(46352246, "Blender Pro", "KitchenMaster", 89.49);
         Product shoes = new Product(59809209, "Running Shoes", "Speedster", 129.99);
+        // Create ProductRepo and add Products to it
+        ProductRepo myProducts = new ProductRepo(new ArrayList<>());
+        myProducts.addProduct(tv);
+        myProducts.addProduct(earbuds);
+        myProducts.addProduct(laptop);
+        myProducts.addProduct(blender);
+        myProducts.addProduct(shoes);
         // Create order
         Order tvAndBlenderOrder = new Order(new Random().nextInt(1000),
                 new HashMap<>(Map.ofEntries(Map.entry(tv.id(), tv), Map.entry(blender.id(), blender))),
                 0.00, "Susi Meier, Hauptstr. 10, 12345 Deppenstedt");
+        // Add price
         tvAndBlenderOrder = tvAndBlenderOrder.orderWithAmount(tv.price() + blender.price());
         // Create another order
         Map<Integer, Product> earbudsShoesBlender = new HashMap<>();
         earbudsShoesBlender.put(earbuds.id(), earbuds);
         earbudsShoesBlender.put(shoes.id(), shoes);
         earbudsShoesBlender.put(blender.id(), blender);
-        double earbudsShoesBlenderAmount = earbuds.price() + shoes.price()+ blender.price();
-        Order earbudsShoesBlenderOrder = new Order(new Random().nextInt(1000), earbudsShoesBlender, earbudsShoesBlenderAmount, "Markus Meier, Sonntagsgasse 10, 34890 Heidenheim" );
-
+        double earbudsShoesBlenderAmount = earbuds.price() + shoes.price() + blender.price();
+        Order earbudsShoesBlenderOrder = new Order(new Random().nextInt(1000), earbudsShoesBlender, earbudsShoesBlenderAmount, "Markus Meier, Sonntagsgasse 10, 34890 Heidenheim");
+        // Add orders to OrderMapRepo
         myOrderMapRepo.getOrderMap().put(tvAndBlenderOrder.id(), tvAndBlenderOrder);
         myOrderMapRepo.addOrder(earbudsShoesBlenderOrder);
-        System.out.println(myOrderMapRepo);
+        tvAndBlenderOrder = tvAndBlenderOrder.removeProductUpdatePrice(tv);
+        tvAndBlenderOrder = tvAndBlenderOrder.addProductUpdatePrice(tv);
+
+
+        Order mariasOrder = new Order(laptop,
+                "Maria Kaufmann, Eschenweg 38b, 34820 Zeusenen");
+        System.out.println(mariasOrder);
+
     }
 }
--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public record Order(
         int id,
@@ -10,8 +11,32 @@ public record Order(
         String deliveryAddress
 ) {
 
+
+    public Order(Product product,
+                 String deliveryAddress) {
+        this(new Random().nextInt(1000),
+                new HashMap<>(Map.ofEntries(Map.entry(product.id(), product))), product.price(), deliveryAddress);
+    }
+
+
+    // die methods sind hier aber irgendwie nicht so sinnvoll weil record..
+    public Order addProductUpdatePrice(Product product) {
+        Map<Integer, Product> newOrderedProducts = orderedProducts;
+        newOrderedProducts.put(product.id(), product);
+        double newAmount = amount + product.price();
+        return new Order(id, newOrderedProducts, newAmount, deliveryAddress);
+    }
+
+    public Order removeProductUpdatePrice(Product product) {
+        Map<Integer, Product> newOrderedProducts = orderedProducts;
+        newOrderedProducts.remove(product.id());
+        double newAmount = amount - product.price();
+        return new Order(id, newOrderedProducts, newAmount, deliveryAddress);
+    }
+
     public Order orderWithAmount(double newAmount) {
         return new Order(id(), orderedProducts(), newAmount, deliveryAddress());
     }
+
 
 }
